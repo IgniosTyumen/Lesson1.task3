27eefbc61bbe6266573c7a1f73f8593ebb7bbe28
IgniosTyumen <rfwbr2@mail.ru>
2019-07-27 22:54:30 +0500

    Initial commit

---
+++ src/Reverse.java
@@ -0,0 +1,9 @@
+public final class Reverse {
+
+
+    // помните, самое главное выполнить задачу! необязательно выполнять ее "честно"
+    public static final String reverse(String input){
+        String result = new StringBuffer(input).reverse().toString();
+        return result;
+    }
+}
